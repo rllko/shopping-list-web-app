88419ef0509caeeec691eefb06eb3bcd71c15a5d
newrlkko <166535715+rllko@users.noreply.github.com>
2024-07-15 23:36:55 +0100

    added grid instead of flex

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 function Footer(){
     return(<footer className="bg-orange-200 flex justify-between px-16 items-center">
         <div>
-            <img className="p-2 l- hover:cursor-pointer" src="./src/assets/img/logo nobg small.png" width={"90px"} alt="" />
+            <img className="p-2 l- hover:cursor-pointer" src="./src/assets/img/logo nobg small.png" width={100} alt="" />
         </div>
-        <p className="text-xs text-center">© CopyRight 2022 - MixList Corporation</p>
+        <p className="text-[1em] text-center">© Copyright 2022 - MixList Corporation</p>
         <button className=
-        "flex text-[10px] bg-transparent justify-center \
-         items-center p-4 px-5 rounded-lg border-2 mr-3 relative \
+        "flex text-[0.8em] my-2 bg-transparent justify-center sm:hidden \
+         items-center px-5 rounded-lg border-2 mr-3 relative \
          w-[100] align-text-top h-[3vw] border-black \
          hover:bg-gradient-to-tr cursor-pointer hover:from-orange-100 \
          shadow-solid hover:to-orange-300 text-md hover:font-bold">
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,19 @@
 function NavBar(){
     return(
     <main className="sticky top-0 bg-white">
-        <header className=" top-0 flex items-center justify-between">
+        <header className=" top-0 flex items-center justify-between mx-10">
             <div>
-                <img className="p-2 hover:cursor-pointer" src="https://github.com/rllko/RecieptsWebApp/blob/master/src/assets/img/logo%20nobg%20small.png?raw=true" width={150} alt="" />
+                <img className="p-2 shover:cursor-pointer" src="./src/assets/img/logo nobg small.png" width={150} alt="" />
             </div>
-            <nav> 
+            <nav className="xl:!flex hidden "> 
                 <div className="flex align-middle gap-4 items-center justify-center">
-                    <div  className="text-black hover:font-bold  hover:cursor-pointer">Pagina Inicial</div>
-                    <div  className="text-black hover:font-bold  hover:cursor-pointer">Minhas Listas</div>
-                    <div  className="text-black hover:font-bold  hover:cursor-pointer">Receitas</div>
-                    <div  className="text-black hover:font-bold  hover:cursor-pointer">Sobre</div>
+                    <div  className="text-lg text-black hover:font-bold  hover:cursor-pointer">Pagina Inicial</div>
+                    <div  className="text-lg text-black hover:font-bold  hover:cursor-pointer">Minhas Listas</div>
+                    <div  className="text-lg text-black hover:font-bold  hover:cursor-pointer">Receitas</div>
+                    <div  className="text-lg text-black hover:font-bold  hover:cursor-pointer">Sobre</div>
                 </div>
             </nav>
-            <button className="flex justify-center items-center rounded-lg border-2 mr-3 relative w-[8vw] align-text-top h-[3vw] border-black hover:bg-gradient-to-tr cursor-pointer hover:from-orange-100 shadow-solid hover:to-orange-300 text-md hover:font-bold">
+            <button className="flex justify-center items-center rounded-lg border-2 mr-3 relative w-[8vw] align-text-top h-full border-black hover:bg-gradient-to-tr cursor-pointer hover:from-orange-100 shadow-solid hover:to-orange-300 text-md hover:font-bold">
                 Login
             </button>
         </header>
