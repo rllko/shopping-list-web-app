25e2bc12889992f983724ff38481216e17a65c5a
newrlkko <166535715+rllko@users.noreply.github.com>
2024-07-15 20:44:44 +0100

    fixes

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,25 +2,25 @@ function Cards(){
     return(
         <>
     <section>
-        <div className="bg-gradient-to-br from-orange-300 justify-items-center to-orange-400 h-full flex flex-wrap p-6 justify-between">
+        <div className="bg-gradient-to-br px-[30%] sm:px-[25%] lg:px-[30%] xl:px-[20%] from-orange-300 justify-items-center to-orange-400 h-full flex flex-wrap p-6 justify-between">
             <div className="">
-                <h2 className="text-white text-3xl w-96 pt-20 font-bold">Organize a sua forma de comprar!</h2>
-                <p className="text-white text-sm w-72 pt-4">Crie listas para organizar as suas compras
+                <h2 className="text-white text-[2em] w-96 pt-20 font-bold">Organize a sua forma de comprar!</h2>
+                <p className="text-white text-lg w-72 pt-4">Crie listas para organizar as suas compras
                 de forma a poupar tempo e dinheiro</p>
             </div>
             <div>
-                <img src="./src/assets/img/app-compras.svg" className="pr-10" width={"100%"} alt="" />
+                <img src="./src/assets/img/app-compras.svg" className="pr-10" width={"400vw"} alt="" />
             </div>
         </div>
     </section>
     <section>
-        <div className="bg-white h-full flex  flex-wrap p-6 justify-between">
+        <div className="bg-white px-[30%] sm:px-[25%] lg:px-[30%] xl:px-[14%] h-full flex flex-wrap p-6 justify-between">
             <div>
-                <img src="./src/assets/img/lista-mulher.svg" className="pr-10 scale-x-[-1]" width={"100%"} alt="" />
+                <img src="./src/assets/img/lista-mulher.svg" className="pr-10 scale-x-[-1]" width={400} alt="" />
             </div>
-            <div className="mr-10">
-                <h2 className="text-black text-3xl w-96 pt-24 font-bold flex">Crie listas de compras <img className="ml-1 mt-[2%]" src="./src/assets/img/list-icon.svg" width={25} alt="" /></h2> 
-                <p className="text-sm text-zinc-600 w-[45vw] pt-2">
+            <div className="mr-3">
+                <h2 className="text-black text-3xl w-70 pt-24 font-bold flex">Crie listas de compras <img className="ml-1 mt-[2%]" src="./src/assets/img/list-icon.svg" width={25} alt="" /></h2> 
+                <p className="text-la text-zinc-600 w-[45vw] pt-2">
                     Tenha a possibilidade de criar listas de compras individuais ou coletivas.
                      Mantenha a ida ao mercado a mais organizada o possivel, tendo uma lista para cada ocasião.
                     Compare os preços entre produtos e lojas diferentes.</p>
@@ -28,16 +28,16 @@ function Cards(){
         </div>
     </section>
     <section>
-        <div className="bg-white h-full flex mt-24 flex-wrap p-6 justify-between">
+        <div className="bg-white px-[30%] sm:px-[25%] lg:px-[30%] xl:px-[17%] h-full flex mt-12 flex-wrap p-6 justify-between">
             <div className="mr-10">
-                <h2 className="text-black text-3xl w-96 font-bold flex">Receitas <img className="ml-1 mt-[2%]" src="./src/assets/img/recipe-icon.svg" width={25} alt="" /></h2> 
-                <p className="text-sm text-zinc-600 w-[39vw] pt-2">
+                <h2 className="text-black text-[2em] w-96 font-bold flex">Receitas <img className="ml-1 mt-[2%]" src="./src/assets/img/recipe-icon.svg" width={25} alt="" /></h2> 
+                <p className="text-md text-zinc-600 w-[39vw] pt-2">
                 Encontre a receita que está a procurar.
 Temos muitas receitas de culinária para todos os níveis, com fotos e explicadas passo a passo.
 Adicione automaticamente os ingredientes necessários para confecionar as suas receitas à sua lista de compras!</p>
             </div>
             <div>
-                <img src="./src/assets/img/receita.svg" className="pr-10" width={"100%"} alt="" />
+                <img src="./src/assets/img/receita.svg" className="pr-10 w-96"  alt="" />
             </div>
         </div>
     </section>
