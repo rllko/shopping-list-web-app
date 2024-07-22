922b8d2bfe979f2ee3426def28c4ebb4f52650aa
newrlkko <166535715+rllko@users.noreply.github.com>
2024-07-23 00:19:51 +0100

    final touches on index

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,47 +1,46 @@
 function Cards(){
     return(
-        <>
-    <section className="bg-gradient-to-br from-orange-300 border-b-[1px] border-zinc-200 sm:border-0 to-orange-400">
-        <div className="flex justify-center pt-1 text-wrap items-center flex-wrap md:flex-nowrap 
-                        xl:mt-0 xl:justify-center mx-auto xl:max-w-7xl 2xl:w-[70%] w-[85%]">
-            <h2 className="text-amber-50 w-92 xl:w-[56%] text-center font-bold mb-5 md:mx-10 sm:mb-0">
-                Organize your next shopping spree!!
-            </h2>
-            <div>
-                <img src="./img/app-compras.svg" className="w-48 xl:w-72" alt="" />
+    <div className="grid grid-rows-3 bg-white ">
+        <section className="bg-gradient-to-br  py-2 md:py-0 from-orange-300 border-b-[1px] border-zinc-200 sm:border-0 to-orange-400">
+            <div className="flex justify-center h-full pt-1 text-wrap items-center flex-wrap md:flex-nowrap 
+                            xl:mt-0 xl:justify-center mx-auto xl:max-w-7xl 2xl:w-[70%] w-[85%]">
+                <h2 className="text-amber-50 w-92 xl:w-[56%] text-center font-bold  md:mx-10 sm:mb-0">
+                    Organize your next shopping spree
+                </h2>
+                <div>
+                    <img src="./img/app-compras.svg" className="w-52 xl:w-72" alt="" />
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
 
-    <section className="bg-white md:pt-2 py-4">
-        <div className="flex justify-center mx-auto items-center flex-wrap
-                        md:flex-nowrap 
-                        xl:justify-center xl:max-w-6xl md:w-[65%] lg:w-[62%] 2xl:w-[60%]">
-            <div className="">
-                <img src="./img/lista-mulher.svg" className="w-48 md:w-full pr-6 scale-x-[-1]" 
-                alt="lista-mulher.svg" />
+        <section className="self-stretch min-h-[45vh]">
+            <div className="flex h-full justify-center mx-auto items-center flex-wrap
+                            md:flex-nowrap 
+                            xl:justify-center xl:max-w-6xl md:w-[65%] lg:w-[62%] 2xl:w-[60%]">
+                <div className="">
+                    <img src="./img/lista-mulher.svg" className="w-52 md:w-full pr-6 scale-x-[-1]" 
+                    alt="lista-mulher.svg" />
+                </div>
+                <div className="flex flex-col items-center">
+                    <p className="text-zinc-600 text-center my-1 text-base  md:text-md w-[60%] mb-2 xl:text-xl lg:text-lg/6 md:pt-4">
+                        Create a digital list to organize your shopping and save time and paper.
+                    </p>
+                    <button className= 
+                    "text-sm mt-4 md:text-xs xl:text-base my-2 bg-transparent lg:block \
+                    px-5 rounded-lg border-2 align-text-top border-black \
+                    hover:bg-gradient-to-tr cursor-pointer text-black hover:from-orange-100 \
+                    shadow-solid hover:to-orange-300 text-md hover:font-bold">
+                        Plan your next journey
+                    </button>
+                </div>
             </div>
-            <div className="flex flex-col items-center">
-                <p className="text-zinc-600 text-center my-1 text-base  md:text-md w-[60%] xl:text-xl lg:text-lg/6 md:pt-4">
-                    Create a digital list to organize your shopping and save time and paper.
-                </p>
-                <button className=
-                "text-sm md:text-xs xl:text-base my-2 bg-transparent lg:block \
-                px-5 rounded-lg border-2 align-text-top border-black \
-                hover:bg-gradient-to-tr cursor-pointer text-black hover:from-orange-100 \
-                shadow-solid hover:to-orange-300 text-md hover:font-bold">
-                    Plan your next journey
-                </button>
-            </div>
-        </div>
-    </section>
-
-    <section className="bg-white md:py-2 py-3 sm:border-0 border-t-[1px] border-zinc-200">
-        <div className="flex justify-center mx-auto items-center flex-wrap
+        </section>
+    <section className="md:py-2 md:m-0">
+        <div className="flex justify-center h-full mx-auto items-center flex-wrap
                         md:flex-nowrap 
                         xl:justify-center xl:max-w-6xl w-[90%] sm:w-full md:w-[65%] lg:w-[62%] 2xl:w-[60%]">
 
-                <p className="text-zinc-600 text-center my-1 md:mx-10 mx-12 lg:mx-0 text-base md:text-md md:w-[55%] xl:px-36 2xl:px-0 xl:w-[70%] 2xl:w-full xl:text-xl lg:text-lg/6 md:pt-4">
+                <p className="text-zinc-600 text-center my-1 md:mx-10 mx-12 lg:mx-0 text-base md:text-md md:w-[55%] xl:px-36 2xl:px-0 xl:w-[70%] 2xl:mx-20 xl:text-xl lg:text-lg/6 md:pt-4">
                     Never run out of ideas with our large list of recipes that range from a simple cake to the best pasta.
                 </p>
             <div className="m-0">
@@ -49,7 +48,7 @@ function Cards(){
             </div>
             </div>
     </section>
-    </>)
+    </div>)
 }
 
 export default Cards;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,20 +4,20 @@ function NavBar(){
     const [isSidebarEnabled,setIsSidebarEnabled] = useState(false);
     return(
     <main className="sticky top-0 z-50 bg-white ">
-        <header className=" flex items-center justify-between w-full xl:max-w-7xl mx-auto">
+        <header className=" flex items-center justify-between w-full max-w-[95%] sm:max-w-2xl md:max-w-5xl xl:max-w-6xl 2xl:max-w-full md:mx-auto">
             <div>
-                <img className="p-2 hover:cursor-pointer" src="./img/logo nobg small.png" width={150} alt="" />
+                <img className="p-2 ml-[.5em] hover:cursor-pointer" src="./img/logo nobg small.png" width={150} alt="" />
             </div>
-            <nav className="md:!flex hidden mx-auto"> 
+            <nav className="md:!flex hidden "> 
                 <div className=" relative flex gap-0 mx-auto items-center justify-center">
-                    <div  className="text-lg text-black hover:font-semibold transition-all duration-100 p-4 hover:bg-orange-100/50  h-full hover:cursor-pointer">Home</div>
-                    <div  className="text-lg text-black hover:font-semibold transition-all duration-100 p-4 hover:bg-orange-100/50 hover:cursor-pointer">My Lists</div>
-                    <div  className="text-lg text-black hover:font-semibold transition-all duration-100 p-4 hover:bg-orange-100/50 hover:cursor-pointer">Recipes</div>
-                    <div  className="text-lg text-black hover:font-semibold transition-all duration-100 p-4 hover:bg-orange-100/50 hover:cursor-pointer">About</div>
+                    <div  className="text-lg text-black hover:font-semibold transition-all duration-100 p-5 hover:bg-orange-100/50  h-full hover:cursor-pointer">Home</div>
+                    <div  className="text-lg text-black hover:font-semibold transition-all duration-100 p-5 hover:bg-orange-100/50 hover:cursor-pointer">My Lists</div>
+                    <div  className="text-lg text-black hover:font-semibold transition-all duration-100 p-5 hover:bg-orange-100/50 hover:cursor-pointer">Recipes</div>
+                    <div  className="text-lg text-black hover:font-semibold transition-all duration-100 p-5 hover:bg-orange-100/50 hover:cursor-pointer">About</div>
                 </div>
             </nav>
-            <div className="flex flex-row items-center grid-cols-2 xl:grid-cols-1">
-                <button className="flex justify-center items-center
+            <div className="  md:flex flex-row items-center">
+                <button className="hidden xl:flex justify-center items-center
                  rounded-lg border-2 mr-2 relative w-[50%] sm:w-[50%] md:w-full
                   align-text-top h-full border-black
                    outline-none text-black bg-white 
@@ -26,14 +26,14 @@ function NavBar(){
                    text-md transition-all duration-200 hover:font-bold">
                     Login
                 </button>
-                <div className="md:hidden flex mx-auto lg:pr-4 right-0 cursor-pointer relative">
-                    <div id="mobile-menu-button" onClick={()=>{ setIsSidebarEnabled(!isSidebarEnabled)}} className={`group ${isSidebarEnabled ? "open" : ""}`}>
+                <div className="md:hidden flex mx-auto lg:pr-4 -right-3 cursor-pointer relative mt-1 mr-[.5em]">
+                    <div id="mobile-menu-button"  onClick={()=>{ setIsSidebarEnabled(!isSidebarEnabled)}} className={`group ${isSidebarEnabled ? "open" : ""}`}>
                         <div className={`bg-black rounded-full relative t w-8 transition-all duration-200 h-1 ${isSidebarEnabled ? "rotate-45 top-2" : ""}`}></div>
                         <div className={`bg-black rounded-full w-8 h-1 transition-all ease-in-out duration-200 my-1 ${isSidebarEnabled ? "-rotate-45 " : ""}`}></div>
                         <div className={`bg-black rounded-full w-8 h-1 ${isSidebarEnabled ? "hidden" :""}`}></div>
                     </div>
                     <div className={`absolute transition-all ${isSidebarEnabled? "block" : "hidden"} z-40 -right-4 w-[calc(100vw+10px)] top-11 text-center bg-white`}>
-                    <div  className="text-lg text-black hover:font-semibold h-12 flex items-center justify-center hover:bg-orange-100 hover:cursor-pointer">Home</div>
+                    <div  className="text-lg text-black hover:font-semibold h-12 flex items-center justify-center hover:bg-orange-100 hover:cursor-pointer">Login</div>
                     <div  className="text-lg text-black hover:font-semibold h-12 flex items-center justify-center hover:bg-orange-100 hover:cursor-pointer">Lists</div>
                     <div  className="text-lg text-black hover:font-semibold h-12 flex items-center justify-center hover:bg-orange-100 hover:cursor-pointer">Recipes</div>
                     <div  className="text-lg text-black hover:font-semibold h-12 flex items-center justify-center hover:bg-orange-100 hover:cursor-pointer">About</div>
